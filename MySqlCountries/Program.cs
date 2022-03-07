06dd3293c3fb5b696997496f9d7bab81a8703530
Ragnar Rästas <rrastas@tktk.ee>
2022-03-07 19:09:29 +0200

    New commit: Week 13 and Week 14

---
+++ MySqlCountries/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace MySqlCountries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadCountries();
+        }
+
+        private static void ReadCountries()
+        {
+            Database databaseObj = new Database();
+
+            string query = "SELECT Country.countryname AS Country, Capital.capitalname as Capital FROM Capital JOIN Country on Capital.countryid = Country.rowid";
+
+            SQLiteCommand myCommand = new SQLiteCommand(query, databaseObj.myConnection);
+            databaseObj.OpenConnection();
+
+            SQLiteDataReader data = myCommand.ExecuteReader();
+
+            if (data.HasRows)
+            {
+                while (data.Read())
+                {
+                    Console.WriteLine($"Country: {data["Country"]}, Capital: {data["Capital"]}");
+                }
+            }
+
+            databaseObj.CloseConnection();
+
+        }
+
+
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace OOP
+{
+    class Program
+    {
+
+        class Dog
+        {
+            string name;
+            int spots;
+            double levelOfHappines;
+
+            //constructor
+            public Dog(string _name, int _spots)
+            {
+                name = _name;
+                spots = _spots;
+                levelOfHappines = 0;
+                Console.WriteLine($"The dog {name} with {spots} spots has been created.");
+            }
+
+            //getter for name
+            public string Name
+            {
+                get { return name; }
+            }
+            //getter for level of happines
+            public double LevelOfHappines
+            {
+                get { return levelOfHappines; }
+            }
+
+            public void Bark()
+            {
+                levelOfHappines += 0.2; //levelOfHappines = levelOfHappines + 0.2
+                Console.WriteLine($"{name} says: woof-woof!");
+            }
+
+            public void Wag()
+            {
+                Console.WriteLine("Wiggle-wiggle!");
+            }
+
+            public void ShowDogData()
+            {
+                Console.WriteLine($"Name: {name}");
+                Console.WriteLine($"Spots: {spots}");
+                Console.WriteLine($"Level of happines: {levelOfHappines}");
+            }
+
+            public void Rename(string newName)
+            {
+                Console.WriteLine($"The dog {name} has been renamed to {newName}");
+                name = newName;
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Dog myDog = new Dog("Nelly", 25);
+
+            Console.WriteLine($"{myDog.Name} is a good boy!");
+
+            //myDog.name = "Good boy";
+            myDog.Rename("Good Boy");
+
+            while (myDog.LevelOfHappines < 1)
+            {
+                myDog.Bark();
+            }
+
+            myDog.Wag();
+
+            myDog.ShowDogData();
+
+        }
+    }
+}
